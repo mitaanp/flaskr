0e2e0112dddb68cbed67b2d4c12d323848f9aced
Mitaan Patil <mitaan.patil@gmail.com>
2023-05-26 10:41:40 -0700

    initial commit

---
+++ hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+#Creating a flask instance
+
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+    first_name = "John"
+    stuff = "This is bold text"
+
+    favorite_pizza = ["Pepperoni", "Cheese", 32, "Mushroom"]
+    return render_template("index.html", first_name=first_name,
+                           stuff=stuff,
+                           favorite_pizza=favorite_pizza)
+
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", user_name=name)
+
+# Create Custom Error Pages
+
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
