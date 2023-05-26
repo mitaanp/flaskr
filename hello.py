0aaf323715448340c3b11824d86d63fbd8959802
Mitaan Patil <mitaan.patil@gmail.com>
2023-05-26 12:12:59 -0700

    What the forms

--- hello.py
+++ hello.py
@@ -14,7 +14,6 @@ class NamerForm(FlaskForm):
     name = StringField("What's your name", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
-
 # Create a route decorator
 @app.route('/')
 
@@ -22,7 +21,7 @@ def index():
     first_name = "John"
     stuff = "This is bold text"
 
-    favorite_pizza = ["Pepperoni", "Cheese", 32, "Mushroom"]
+    favorite_pizza = ["Chicken", "Cheese", 32, "Mushroom"]
     return render_template("index.html", first_name=first_name,
                            stuff=stuff,
                            favorite_pizza=favorite_pizza)
@@ -43,6 +42,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("name.html",
+                           name=name,
+                           form=form)
 
 
 
